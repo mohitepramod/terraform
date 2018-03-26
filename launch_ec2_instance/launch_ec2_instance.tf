e1bf0ef698b7f8b5cc19ccb7fe814b6dc84a6710
mohitepramod <30810865+mohitepramod@users.noreply.github.com>
2018-03-26 16:25:55 +0530

    Update launch_ec2_instance.tf

--- launch_ec2_instance/launch_ec2_instance.tf
+++ launch_ec2_instance/launch_ec2_instance.tf
@@ -1,14 +1,14 @@
 provider "aws" {
-        access_key = "AKIAJ57E5ATJL6TQRQFQ"
-        secret_key = "64+5o3LxMX+o7aGHlk3ibchBrAYaCpiC/lWLnSg7"
-        region = "us-east-1"
+        access_key = "Your_Access_Key"
+        secret_key = "Your_Secret_Key"
+        region = "Your_region"
 
 }
 
 resource "aws_instance" "instanceName"{
-        ami = "ami-1853ac65"
+        ami = "ami-1234567"
         instance_type = "t2.micro"
-        key_name = "samplekeyPM-1"
+        key_name = "Your_key"
         security_groups = ["default"]
-        tags { Name = "terraform-instance" }
+        tags { Name = "InstanceName" }
 }
