c874eaacce224c3f72fa268d9bb9e15a948b24c1
mohitepramod <30810865+mohitepramod@users.noreply.github.com>
2018-03-26 16:22:54 +0530

    Update snapshotOfVolume.tf

--- snapshotOfVolume/snapshotOfVolume.tf
+++ snapshotOfVolume/snapshotOfVolume.tf
@@ -1,15 +1,15 @@
 resource "aws_ebs_volume" "ebs_volume" {
-availability_zone = "us-east-1a"
+availability_zone = "your_availability_zone"
 size = 2
 tags {
-Name = "New-PM-Volume"
+Name = "give_name_to_Volume"
 }
 }
 
 resource "aws_ebs_snapshot" "ebs_snapshot" {
 volume_id = "${aws_ebs_volume.ebs_volume.id}"
 tags {
-Name = "ebs-snapshot-PM-2018"
+Name = "give_name_to_ebs_snapshot"
 }
 depends_on = ["aws_ebs_volume.ebs_volume"]
 }
